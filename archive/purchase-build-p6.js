4215bb81d1a853c103e22bff6d3f9b25f3155a40
thiraphong <terapong4100@gmail.com>
2026-09-07 15:28:43 +0700

    feat: improve purchase dashboard and navigation

---
+++ archive/purchase-build-p6.js
@@ -0,0 +1,6 @@
+(function () {
+  var script = document.createElement('script');
+  script.src = './bms-purchase.js?build=20260828-p6-' + Date.now();
+  script.async = false;
+  document.head.appendChild(script);
+})();
